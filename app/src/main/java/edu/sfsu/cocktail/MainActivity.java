4f1ca10d97bf98d45cf24da5a0007742b0cc3144
weatherpub <patrick@bayareamedia.net>
2023-11-04 20:56:01 -0700

    The model - CocktailModel has been built.

--- app/src/main/java/edu/sfsu/cocktail/MainActivity.java
+++ app/src/main/java/edu/sfsu/cocktail/MainActivity.java
@@ -1,14 +1,28 @@
 package edu.sfsu.cocktail;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+import edu.sfsu.cocktail.Models.CocktailModel;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<CocktailModel> cocktailModel = null;
+    ProgressBar progressBar;
+    RecyclerView recyclerView;
+    private static final String api = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cocktailModel = new ArrayList<>();
+        // recyclerView = findViewById(R.id.recyclerView);
+
+        // new cocktailTask(this, recyclerView, progressBar, cocktailModel);
     }
 }
