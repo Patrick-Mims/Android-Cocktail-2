fdcf348dd79d1d2e4eac34901f8fe4d1ad03660f
weatherpub <patrick@bayareamedia.net>
2023-12-06 11:20:43 -0800

    Updated the Navigation Drawer functionality, when the back key is hit the drawer closes, rather than close the app.

--- app/src/main/java/edu/sfsu/cocktail/MainActivity.java
+++ app/src/main/java/edu/sfsu/cocktail/MainActivity.java
@@ -226,6 +226,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+           drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
         super.onPointerCaptureChanged(hasCapture);
     }
